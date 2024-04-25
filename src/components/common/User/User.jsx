cd91ed598f976541442602e8c7b947fd0281130e
beingshahbaj <130038256+beingshahbaj@users.noreply.github.com>
2024-04-25 16:19:11 +0530

    update

--- src/components/common/User/User.jsx
+++ src/components/common/User/User.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
